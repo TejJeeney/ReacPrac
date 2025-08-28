0dd2ea433df4b1d6886411facc1218e782e0bc40
TejJeeney <edutejasva@gmail.com>
2025-08-29 02:29:16 +0530

    New Prac

---
+++ TailwindProps03/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card.jsx'
+
+
+function App() {
+return (
+  <>
+    <div className='font-bold bg-yellow-400 rounded'>This is a Tailwind CSS example</div>
+  </>
+  )
+}
+
+export default App
