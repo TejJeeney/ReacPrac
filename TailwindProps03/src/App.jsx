a096d4203cb9dec04564bb9d030a203fe00827e9
TejJeeney <edutejasva@gmail.com>
2025-08-29 20:38:53 +0530

    ....

--- TailwindProps03/src/App.jsx
+++ TailwindProps03/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Card from './components/card.jsx'
+import Card from './components/Card.jsx'
 
 
 function App() {
 return (
   <>
-    <div className='font-bold bg-yellow-400 rounded'>This is a Tailwind CSS example</div>
+    <h1 className='font-bold bg-yellow-400 rounded'>This is a Tailwind CSS example</h1>
+    <Card />
   </>
   )
 }
--- TailwindProps03/src/main.jsx
+++ TailwindProps03/src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Card9 from './App.jsx'
-import Card from './components/card.jsx'
+import Card from './components/Card.jsx'
 
 createRoot(document.getElementById('root')).render(
   <>
-    <Card />
+    <App />
+    {/* <Card /> */}
   </>,
 )
