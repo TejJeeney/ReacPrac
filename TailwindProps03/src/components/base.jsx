1ef3758548a7d4049195dc3c9bff09130064e0ee
TejJeeney <edutejasva@gmail.com>
2025-09-02 02:27:36 +0530

    Impact button

--- TailwindProps03/src/components/base.jsx
+++ TailwindProps03/src/components/base.jsx
@@ -5,6 +5,7 @@ export default function Base({username, address="partsUnknown"}) {
         <>
         <h1 className='text-2xl font-bold'>Hi there, {username}</h1>
         <h2 className='text-xl'>We know you are from {address}</h2>
+        <h3 className='bg-blue-300 text-center tracking-tight text-3xl text-black line-through italic rounded-3xl'>This is a base component</h3>
         </>
     )
 }
