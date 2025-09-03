05b9a365ea3fe6f03af191c9f7e2a8c6e82e17d4
TejJeeney <edutejasva@gmail.com>
2025-09-03 23:45:18 +0530

    Fixes

--- TailwindProps03/src/components/base.jsx
+++ TailwindProps03/src/components/base.jsx
@@ -6,7 +6,8 @@ export default function Base({username, address="partsUnknown"}) {
         <h1 className='text-2xl font-bold'>Hi there, {username}</h1>
         <h2 className='text-xl'>We know you are from {address}</h2>
         <h3 className='bg-blue-300 text-center tracking-tight text-3xl text-black line-through italic rounded-3xl'>This is a base component</h3>
+        <div className='bg-slate-300 p-4 rounded-xl tracking-wider flex flex-wrap justify-center'> Hello this is a simple div to show some Tailwind CSS classes in action.</div>
         </>
-    )
+    ) 
 }
 
