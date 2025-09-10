bd06fd91e04a63b277b8c945945c3a615055669e
TejJeeney <edutejasva@gmail.com>
2025-09-10 21:21:25 +0530

    Few implementations

---
+++ reactRouter07/src/components/User/User.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useParams } from 'react-router'
+import App from '../../App'
+
+
+function User() {
+    const {let_id} = useParams() 
+  return (
+    <>
+    <div>User: {let_id} </div>
+    <App />
+    </>
+  )
+}
+
+export default User
--- reactRouter07/src/main.jsx
+++ reactRouter07/src/main.jsx
@@ -2,40 +2,53 @@ import { Children, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter,createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
+import { Route } from 'react-router'
+import User from './components/User/User.jsx'
 
+// const router = createBrowserRouter([
 
-const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+//       {
+//         path: "app",
+//         element: <App />
+//       },
+//       {
+//         path: 'contact',
+//         element: <Contact />
+//       }
+//     ]
 
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <Home />
-      },
-      {
-        path: "about",
-        element: <About />
-      },
-      {
-        path: "app",
-        element: <App />
-      },
-      {
-        path: 'contact',
-        element: <Contact />
-      }
-    ]
+//   }
+// ])
 
-  }
-])
+const router = createBrowserRouter(
+  createRoutesFromElements(
 
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:let_id' element={<User />} /> 
+    </Route>
+
+  )
+) 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
