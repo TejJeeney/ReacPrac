bfa779d1c0d915927f54e948321e98b218beae25
TejJeeney <edutejasva@gmail.com>
2025-09-11 00:57:22 +0530

    Add Github profile page and route
    Introduced a new Github component that fetches and displays user profile data from the GitHub API. Updated the Header navigation to include a link to the new Github page and registered the route in the main router configuration.

---
+++ reactRouter07/src/components/Github/Github.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+
+
+
+function Github() {
+    const [data, setData] = React.useState({})
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/TejJeeney')
+            .then((res) => res.json())
+            .then(data => {
+                console.log(data)
+                setData(data)
+            })
+    }, [])
+
+    return (
+        <>
+            <h1 className="text-2xl font-bold text-gray-600 m-4 p-4 center"> Your Github Profile </h1>
+            <div>Github followers: {data.followers}
+                <img className='rounded-full m-4' src={data.avatar_url} alt="profile pic" width="100" height="100" />
+            </div>
+        </>
+    )
+}
+
+export default Github
