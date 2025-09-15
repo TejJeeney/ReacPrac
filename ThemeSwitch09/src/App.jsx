8fc686ddd3e1c8aabf98d48d901ee75603c994b7
TejJeeney <edutejasva@gmail.com>
2025-09-15 18:35:28 +0530

    Few Minor bugs issue
    persistent tailwind dark issue

--- ThemeSwitch09/src/App.jsx
+++ ThemeSwitch09/src/App.jsx
@@ -7,26 +7,25 @@ import Card from './components/Card/Card'
 
 function App() {
 
-  const [themeMode, setThemeMode] = useState("dark")
+  const [themeMode, setThemeMode] = useState("light")
 
   const lightTheme = () => {
     setThemeMode("light")
   }
 
-  const darkTheme = () => {
+  function darkTheme() {
     setThemeMode("dark")
   }
 
   useEffect(() => {
-    document.querySelector('html').classList.remove("light" , "dark")
+    document.querySelector('html').classList.remove("light", "dark")
     document.querySelector('html').classList.add(themeMode)
   }, [themeMode])
 
 
 
   return (
-    <ThemeProvider values={{ themeMode, lightTheme, darkTheme }}>
-
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
