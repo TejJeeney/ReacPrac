cdd14e370b5f868b80f29f06a4eb205a98afda38
TejJeeney <edutejasva@gmail.com>
2025-09-16 18:23:58 +0530

    Add TodoItem component and integrate in App
    Created the TodoItem component for displaying and editing individual todos. Updated App.jsx to render TodoItem for each todo and import TodoForm and TodoItem from the components index. Added an index.js file to export TodoForm and TodoItem for easier imports.

--- ToDoContextLocal10/src/App.jsx
+++ ToDoContextLocal10/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { TodoContext, TodoProvider } from './contexts'
+import { TodoForm, TodoItem } from './components'
+
+
 
 function App() {
   const [todos, setTodos] = useState([])
@@ -37,14 +38,23 @@ function App() {
 
   return (
     <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
-      <div className="bg-[#172842] min-h-screen py-8">
-        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
-          <div className="mb-4">
-            {/* Todo form goes here */}
-          </div>
-          <div className="flex flex-wrap gap-y-3">
-            {/*Loop and Add TodoItem here */}
+      <div className='min-h-screen w-full bg-[#111d2f]'>
+
+        <div className="bg-[#0f1e35] min-h-screen py-8">
+          <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+            <h1 className="text-2xl font-bold text-center mb-8 mt-2">My Daily Todos</h1>
+            <div className="mb-4">
+              {/* Todo form goes here */}
+              <TodoForm />
+            </div>
+            <div className="flex flex-wrap gap-y-3">
+              {/*Loop and Add TodoItem here */}
+              {todos.map((todo) => (
+                <div key={todo.id} className="w-full">
+                  <TodoItem todo={todo} />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
