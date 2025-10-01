665dee132f4f64c5105632524b75de54fcbadfd8
TejJeeney <edutejasva@gmail.com>
2025-10-01 13:31:39 +0530

    Added button and input components to project

--- MyPracProject/src/components/header/header.jsx
+++ MyPracProject/src/components/header/header.jsx
@@ -1,11 +1,82 @@
 import React from 'react'
+import { Container, Logo, LogoutBtn }  from '../import'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
-function Header() {
+
+
+function Header() { 
+  const authStatus = useSelector((state) => state.auth.status)
+  const navigate = useNavigate()
+
+  const navItems = [
+    {
+      name: 'Home',
+      slug: '/',
+      active: true
+    },
+    {
+      name: 'Login',
+      slug: '/login',
+      active: !authStatus
+    },
+    {
+      name: 'Signup',
+      slug: '/sign-up',
+      active: !authStatus
+    },
+    {
+      name: 'All posts',
+      slug: '/all-posts',
+      active: authStatus
+    },
+    {
+      name: 'Add Post',
+      slug: '/add-post',
+      active: authStatus
+    }
+
+  ]
+
   return (
-    <div className='w-full p-3 m-4 bg-yellow-200 flex justify-center items-center'>
-      <h1 className="text-2xl font-bold text-blue-600">This is the Header</h1>
-      <div>Header</div>
-      </div>
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo width='70px' />
+            </Link>
+          </div>
+
+          <ul className='flex ml-auto'>
+            {navItems.map((item) => 
+            item.active ? (
+              
+              <li key={item.name}>
+                <button
+                onClick={() => navigate(item.slug)}
+                className='inline-block px-6 py-2 duration-200 hover:bg-pink-400 rounded-full'
+                >
+                  {item.name}
+                </button>
+              </li>
+            
+            ) : null 
+            )}
+            
+            {/* agar ye true hoga tabhi show hoga */}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+
+
+          </ul>
+        </nav>
+      </Container>
+    </header>
+
   )
 }
 
